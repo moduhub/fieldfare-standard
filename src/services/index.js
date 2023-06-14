57cd42cafb0aa798f8c29060d0123c685db64864
Adan Kvitschal <adan@moduhub.com>
2023-06-14 18:38:35 -0300

    split from core, updated imports

---
+++ src/services/index.js
@@ -0,0 +1,8 @@
+/**
+ * Fieldfare: Backend framework for distributed networks
+ *
+ * Copyright 2021-2023 Adan Kvitschal
+ * ISC LICENSE
+ */
+
+export {uuid as snapshotServiceUUID, SnapshotService} from './Snapshot.js';
