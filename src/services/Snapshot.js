e46aeee6802907d051ad7457e4dad6ef0269032f
Adan Kvitschal <adan@moduhub.com>
2023-07-05 15:04:42 -0300

    [misc] better store() process logging

--- src/services/Snapshot.js
+++ src/services/Snapshot.js
@@ -11,33 +11,41 @@ export {SnapshotService as implementation};
 
 export const uuid = 'd2b38791-51af-4767-ad08-2f9f1425e90e';
 
+var gNumCalls=0;
+
 export class SnapshotService extends LocalService {
 
     async store(remoteHost, stateMessage) {
+        gNumCalls++;
         //1) Check if remoteHost belongs to the enviroment
-        // if(await this.environment.belongs(remoteHost) === false) {
-        //     throw Error('host does not belong to the environment');
-        // }
+        if(await this.environment.getServicesForHost(remoteHost) === false) {
+            throw Error('host does not belong to the environment');
+        }
 
-        logger.debug('[STORE] Called by ' + remoteHost.id);
+        logger.debug('[STORE-C'+gNumCalls+'] Called by ' + remoteHost.id);
         //2) Check if provided state is valid
-        Utils.validateParameters(stateMessage, ['service', 'data', 'signature', 'source']);
-        Utils.validateParameters(stateMessage.data, ['id', 'ts', 'collections']);
+        try{
+            Utils.validateParameters(stateMessage, ['service', 'data', 'ts', 'signature', 'source']);
+            Utils.validateParameters(stateMessage.data, ['id', 'ts', 'collections']);
+        } catch(e) {
+            logger.debug('[STORE-C'+gNumCalls+'] Message validation failed: ' + e.message);
+            throw Error(e.message);
+        }
         const timeDelta = Date.now() - stateMessage.data.ts;
         if(timeDelta > 1000 * 60 * 60 * 24) {   //a day
-            logger.debug('[STORE] Rejected: state is too old');
+            logger.debug('[STORE-C'+gNumCalls+'] Rejected: state is too old');
             throw Error('state is too old');
         }
         const signatureIsValid = await cryptoManager.verifyMessage(stateMessage, remoteHost.pubKey);
         if(!signatureIsValid) {
-            logger.debug('[STORE] Rejected: sigature is invalid');
+            logger.debug('[STORE-C'+gNumCalls+'] Rejected: sigature is invalid');
             throw Error('signature is invalid');
         }
 
         //3) Check if provided state is newer than previous
         const hostStates = await this.collection.getElement('hostStates');
         if(!hostStates) {
-            logger.debug('[STORE] Internal error: hostStates is null');
+            logger.debug('[STORE-C'+gNumCalls+'] Internal error: hostStates is null');
             throw Error('hostStates is null');
         }
         const keyChunk = Chunk.fromIdentifier(HostIdentifier.toChunkIdentifier(remoteHost.id));
@@ -57,29 +65,34 @@ export class SnapshotService extends LocalService {
                                 stateIsDifferent=true;
                         } else
                         if(prevState.data.collections[uuid] !== stateMessage.data.collections[uuid]) {
-                    stateIsDifferent = true;
+                                stateIsDifferent = true;
                                 status = ' [change]';
                         } else {
                                 status = ' [stale]';
                         }
                         logger.debug('[STORE-C'+gNumCalls+']' + uuid + ' => ' + stateMessage.data.collections[uuid] + status);
-            }
+                }
         }
         if((stateIsNewer && stateIsDifferent) || !prevState) {
             const stateChunk = await Chunk.fromObject(stateMessage);
             stateChunk.ownerID = remoteHost.id;
             //4) Clone complete stat
-            const startTime = Date.now();
-            const numClonedChunks = await stateChunk.clone();
-            const elapsedTime = Date.now() - startTime;
-            logger.debug('SnapshotService.store() cloned ' + numClonedChunks + ' chunks in ' + elapsedTime + 'ms');
+            try {
+                const startTime = Date.now();
+                const numClonedChunks = await stateChunk.clone();
+                const elapsedTime = Date.now() - startTime;
+                logger.debug('[STORE-C'+gNumCalls+'] SnapshotService.store() cloned ' + numClonedChunks + ' chunks in ' + elapsedTime + 'ms');
+            } catch (error) {
+                logger.debug('[STORE-C'+gNumCalls+'] SnapshotService.store() failed to clone state: ' + error.message);
+                throw Error('SnapshotService.store() failed to clone state: ' + error.message);
+            }
             //5) Store state as latest
             await hostStates.set(keyChunk, stateChunk);
             await this.collection.updateElement('hostStates', hostStates.descriptor);
-            logger.debug('[STORE] state updated');
+            logger.debug('[STORE-C'+gNumCalls+'] state updated');
             return 'ok';
         }
-        logger.debug('[STORE] state is stale');
+        logger.debug('[STORE-C'+gNumCalls+'] state is stale');
         return 'state is stale';
     }
 
